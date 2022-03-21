657b25a843a25e79f38316a95db17dc5f93bafb2
Vinicius Pereira Faria <viniciusp.faria@hotmail.com>
2022-03-21 16:53:24 -0300

    *Change projeto to new input and setup basic click action

---
+++ Assets/_KoolGames/Script/Inputs/InputToAction.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace KoolGames
+{
+    public class InputToAction : MonoBehaviour
+    {
+        public static Action<Vector2> onClick;
+        // Start is called before the first frame update
+        private MainLoopInputs _inputActions;
+        void Start()
+        {
+            _inputActions = new MainLoopInputs();
+            _inputActions.Enable();
+
+            _inputActions.BasicMap.OnClick.performed += OnClickPerformed;
+        }
+
+        private void OnClickPerformed(CallbackContext context)
+        {
+            Vector2 clickPosition = _inputActions.BasicMap.OnClick.ReadValue<Vector2>();
+            onClick?.Invoke(clickPosition);
+        }
+
+        
+    }
+}
