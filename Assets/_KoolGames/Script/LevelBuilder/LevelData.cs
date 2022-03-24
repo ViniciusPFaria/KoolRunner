89079c1aeea56d42a02e2e845f4a8ab5ae5a19ad
Vinicius Pereira Faria <viniciusp.faria@hotmail.com>
2022-03-24 08:18:05 -0300

    Fix Unity losing Scriptable data

--- Assets/_KoolGames/Script/LevelBuilder/LevelData.cs
+++ Assets/_KoolGames/Script/LevelBuilder/LevelData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 
 namespace KoolGames
@@ -8,12 +9,13 @@ namespace KoolGames
     [CreateAssetMenu(fileName = "Level", menuName = "KoolGames/Level", order = 1)]
     public class LevelData : ScriptableObject
     {
-        [ShowInInspector][SerializeField] private List<MapObjectData> _levelMapObjectsList = new List<MapObjectData>();    
+        [ShowInInspector][SerializeField] private List<MapObjectData> _levelMapObjectsList;
 
         //set level data
         public void SetLevelData(List<MapObjectData> levelMapObjectsList)
         {
-            this._levelMapObjectsList = levelMapObjectsList;
+            this._levelMapObjectsList = new List<MapObjectData>(levelMapObjectsList);
+            EditorUtility.SetDirty(this);
         }
 
         public List<MapObjectData> GetLevelData()
